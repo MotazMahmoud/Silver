3355e1b3de1c9b06992a7b8764da7eed049aa6de
MotazMahmoud <50845789+MotazMahmoud@users.noreply.github.com>
2019-11-22 15:25:33 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -210,12 +210,12 @@ client.on('message', fkk => {
 
 client.on('ready', () => {
    console.log(`----------------`);
-      console.log(`Cyhper Script By : DREAM`);
+      console.log(`Cyhper Script By : Clover`);
         console.log(`----------------`);
-      console.log(`ON ${client.guilds.size} Servers '     Script By : DREAM ' `);
+      console.log(`ON ${client.guilds.size} Servers '     Script By : CloverChannel ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`Light Games`,"http://twitch.tv/Light")
+client.user.setGame(`والله مدري وش الوضع")
 client.user.setStatus("dnd")
 });
 
